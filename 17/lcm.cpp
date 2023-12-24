f0009d84715a960f7d0345589b81bf09e8313316
Zhentarim <54887323+zhentilar@users.noreply.github.com>
2023-12-24 12:42:19 +0300

    new files

---
+++ 17/lcm.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, max;
+
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+    
+    //Maximum value between n1 and n2 is stored in max
+    max = (n1 > n2) ? n1 : n2;
+
+    do
+    {
+        if (max % n1 == 0 && max % n2 == 0)
+        {
+            cout << "LCM = " << max;
+            break;
+        }
+        else
+            ++max;
+    } while (true);
+
+}
---
+++ 19/power2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() 
+{
+    float base, exponent, result;
+
+    cout << "Enter base and exponent respectively:  ";
+    cin >> base >> exponent;
+
+    result = pow(base, exponent);
+
+    cout << base << "^" << exponent << " = " << result;
+    
+}
